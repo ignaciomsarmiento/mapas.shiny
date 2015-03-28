6c4f26828734debd49ea834f696e3858b717831b
ignaciomsarmiento <ignaciomsarmiento@gmail.com>
2015-03-28 16:26:55 -0500

    Changed codes and hover

--- server.R
+++ server.R
@@ -5,13 +5,13 @@ library(RCurl)
 
 dat<-read.csv("data/data.csv")
 dat<-dat[,-3]
-names(dat)<-c("province","Desocupados","Mujeres", "Criteria.ID",	"Name","Name_orig",	"Canonical Name",	"Parent ID", 	"Country Code",	"Target Type",	"Status")
+names(dat)<-c("province","Desocupados","Mujeres", "Criteria.ID",	"Code","Name", "Name_orig",	"Canonical Name",	"Parent ID", 	"Country Code",	"Target Type",	"Status")
 dat<-data.frame(dat)
 
 
 shinyServer(function(input, output) {
   
   output$gvis <- renderGvis({
-    gvisGeoChart(dat, locationvar="Name", input$var, options=list(region="AR", displayMode="region", resolution="provinces", width=600, height=400))  
+    gvisGeoChart(dat, locationvar="Code", input$var, hovervar="Name", options=list(region="AR", displayMode="region", resolution="provinces", width=600, height=400))  
   })
 })
