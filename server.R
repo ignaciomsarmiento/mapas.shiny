bb6a0bf61bbc31884972ad9fb077968f05c43307
ignaciomsarmiento <ignaciomsarmiento@gmail.com>
2015-04-02 13:10:01 -0500

    Added new data, and redid the panels

--- server.R
+++ server.R
@@ -1,17 +1,50 @@
 require(googleVis)
 require(shiny)
+require(xlsx)
 
 library(RCurl)
+dat<-read.xlsx("data/Inversion.xlsx",sheetName = "Inversion", header=TRUE)
+dat<-dat[,c(-3,-4,-17,-18,-19)]
 
-dat<-read.csv("data/data.csv")
-dat<-dat[,-3]
-names(dat)<-c("province","Desocupados","Mujeres", "Criteria.ID",	"Code","Name", "Name_orig",	"Canonical Name",	"Parent ID", 	"Country Code",	"Target Type",	"Status")
+names(dat)<-c("Code","Name","y.2003","y.2004","y.2005", "y.2006","y.2007","y.2008","y.2009","y.2010","y.2011","y.2012","y.2013","y.2014") 
+
+dat<-reshape(dat, varying=3:14, direction="long")
 dat<-data.frame(dat)
+names(dat)<-c("Code","Name","Year","Inversion","id")
 
 
 shinyServer(function(input, output) {
+  data<-reactive({
+    a <- subset(dat, Year==input$year)
+    a<-droplevels(a)
+    return(a)
+  })
   
   output$gvis <- renderGvis({
-    gvisGeoChart(dat, locationvar="Code", input$var, hovervar="Name", options=list(region="AR", displayMode="region", resolution="provinces", width=600, height=400,  colorAxis="{colors:['#FFFFFF', '#0000FF']}"))  
+    gvisGeoChart(data(), locationvar="Code", colorvar="Inversion", hovervar="Name", options=list(region="AR", displayMode="region", resolution="provinces", width=600, height=400,  colorAxis="{colors:['#FFFFFF', '#0000FF']}"))  
   })
+  
+  data<-reactive({
+    a <- subset(dat, Year==input$year)
+    a<-droplevels(a)
+    return(a)
+  })
+  
+  output$gvis <- renderGvis({
+    gvisGeoChart(data(), locationvar="Code", colorvar="Inversion", hovervar="Name", options=list(region="AR", displayMode="region", resolution="provinces", width=600, height=400,  colorAxis="{colors:['#FFFFFF', '#0000FF']}"))  
+  })
+  
+  
+  data2<-reactive({
+    a <- subset(dat, Code==input$provincia)
+    a<-droplevels(a)
+    return(a)
+  })
+  
+  output$bar <- renderGvis({  
+    gvisColumnChart(data2(), xvar="Year", yvar="Inversion", options=list(hAxis="{title: 'Año', format: '#,###'}", vAxis="{title:'Montos anuales de anuncios de inversión'}",width=600, height=400,  colorAxis="{colors:['#FFFFFF', '#0000FF']}"))
+  })
+  
+
 })
+
--- ui.R
+++ ui.R
@@ -1,17 +1,65 @@
 require(shiny)
 
 shinyUI(pageWithSidebar(
-  headerPanel("Ejemplo: Creando un mapa con Geo Chart"),
+  headerPanel("Montos anuales  de anuncios de inversión por provincia 2003 - 2014*(mill u$s)"),
   sidebarPanel(
-    selectInput("var", 
-                label = "Elija una variable (en %)",
-                choices = list("Desocupados","Mujeres"),
-                selected = "Desocupados")
+    conditionalPanel(condition="input.conditionedPanels==1",
+                     selectInput("year", 
+                                 label = "Elija un Año",
+                                 choices = list("2003"= 2003,
+                                                "2004"=2004,
+                                                "2005"=2005,
+                                                "2006"=2006,
+                                                "2007"=2007,
+                                                "2008"=2008,
+                                                "2009"=2009,
+                                                "2010"=2010,
+                                                "2011"=2011,
+                                                "2012"=2012,
+                                                "2013"=2013,
+                                                "2014"=2014),
+                                 selected = 2003)
+                     
+    ),
+    conditionalPanel(condition="input.conditionedPanels==2",
+                     selectInput("provincia", 
+                                 label = "Elija una Provincia",
+                                 choices = list("Autonomous City of Buenos Aires"="AR-C",
+                                                "Buenos Aires"="AR-B",
+                                                "Catamarca"="AR-K",
+                                                "Cordoba"="AR-X",
+                                                "Corrientes"="AR-W",
+                                                "Chaco"="AR-H",
+                                                "Chubut"="AR-U",
+                                                "Entre Rios"="AR-E",
+                                                "Formosa"="AR-P",
+                                                "Jujuy"="AR-Y",
+                                                "La Pampa"="AR-L",
+                                                "La Rioja"="AR-F",
+                                                "Mendoza"="AR-M",
+                                                "Misiones"="AR-N",
+                                                "Neuquen"="AR-Q",
+                                                "Rio Negro"="AR-R",
+                                                "Salta"="AR-A",
+                                                "San Juan"="AR-J",
+                                                "San Luis"="AR-D",
+                                                "Santa Cruz"="AR-Z",
+                                                "Santa Fe"="AR-S",
+                                                "Santiago del Estero"="AR-G",
+                                                "Tucuman"="AR-T",
+                                                "Tierra del Fuego"="AR-V",
+                                                "Malvinas"="FK"),
+                                 selected = "AR-C")
+     
+    
+    )
   ),
   
   mainPanel(
-    h3(textOutput("year")), 
-    htmlOutput("gvis")
-  )
+    tabsetPanel(
+      tabPanel("Mapas de Inversión", h4("Mapas de Inversión"), htmlOutput("gvis"), value=1),
+      tabPanel("Evolución Inversión por Provincia", h4("Evolución Inversión por Provincia"), htmlOutput("bar"), value=2),
+      id="conditionedPanels"
+    )
 )
-)
+))
